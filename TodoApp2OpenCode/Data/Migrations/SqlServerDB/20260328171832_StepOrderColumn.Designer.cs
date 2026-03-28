d650744b699c4487bc815c798c9c88fa906dd844
Dayniel Lazaro <mdayniel642@gmail.com>
2026-03-28 15:32:46 -0400

    fix calendar days width

--- TodoApp2OpenCode/Data/Migrations/SqlServerDB/20260328171832_StepOrderColumn.Designer.cs
+++ TodoApp2OpenCode/Data/Migrations/SqlServerDB/20260328171832_StepOrderColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoApp2OpenCode.Data;
 
@@ -11,9 +12,11 @@ using TodoApp2OpenCode.Data;
 namespace TodoApp2OpenCode.Data.Migrations.SqlServerDB
 {
     [DbContext(typeof(SqlServerDbContext))]
-    partial class SqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260328171832_StepOrderColumn")]
+    partial class StepOrderColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TodoApp2OpenCode/Data/Migrations/SqlServerDB/20260328171832_StepOrderColumn.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp2OpenCode.Data.Migrations.SqlServerDB
+{
+    /// <inheritdoc />
+    public partial class StepOrderColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Steps_ItemId",
+                table: "Steps");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Steps",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_ItemId_Order",
+                table: "Steps",
+                columns: new[] { "ItemId", "Order" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Steps_ItemId_Order",
+                table: "Steps");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Steps");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_ItemId",
+                table: "Steps",
+                column: "ItemId");
+        }
+    }
+}
