a680a756f4d836c5877935bd1a6f7c3f6964aeaa
Dayniel Lazaro <mdayniel642@gmail.com>
2026-04-08 04:22:34 -0400

    sql migrations

---
+++ TodoApp2OpenCode/Data/Migrations/SqlServerDB/20260407201013_Notifications2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp2OpenCode.Data.Migrations.SqlServerDB
+{
+    /// <inheritdoc />
+    public partial class Notifications2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ParticipantsJson",
+                table: "CalendarEvents",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ParticipantsJson",
+                table: "CalendarEvents",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+        }
+    }
+}
---
+++ TodoApp2OpenCode/Data/Migrations/SqlServerDB/20260408032236_Permissions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp2OpenCode.Data.Migrations.SqlServerDB
+{
+    /// <inheritdoc />
+    public partial class Permissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParticipantPermissionsJson",
+                table: "Boards",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParticipantPermissionsJson",
+                table: "Boards");
+        }
+    }
+}
